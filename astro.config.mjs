2c697804ad91fa57a5eae799a91d48218a2cb6d5
Sebastian Castano <sebastian.caja@gmail.com>
2022-08-27 20:10:56 -0500

    remove the base from the defineConfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://valdemird.github.io',
-	base: '/personal-blog',
 	integrations: [mdx(), sitemap()],
 });
